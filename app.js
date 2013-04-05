bc8b0a9e5fc2eb2425e903c8a66b78a9f1be5c40
John Schimmel <john.schimmel@gmail.com>
2013-04-04 23:29:44 -0400

    twilio sms working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
 
 // the ExpressJS App
@@ -35,8 +35,8 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 
   // database - skipping until week 5
-  app.db = mongoose.connect(process.env.MONGOLAB_URI);
-  console.log("connected to database");
+  // app.db = mongoose.connect(process.env.MONGOLAB_URI);
+  // console.log("connected to database");
   
 });
 
@@ -44,25 +44,12 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-/* 
-SKIPPING FOR FUTURE CLASSES
-SESSIONS w/ MongoDB (store sessions across multiple dynos)
-COOKIEHASH in your .env file (also share with heroku) 
-*/
-// app.use(express.cookieParser(process.env.COOKIEHASH));
-// app.use(express.session({ 
-//     store: new mongoStore({url:process.env.MONGOLAB_URI, maxAge: 300000})
-//     , secret: process.env.COOKIEHASH
-//   })
-// );
 
 // ROUTES
 
 var routes = require('./routes/index.js');
 
 app.get('/', routes.index);
-app.get('/data/places', routes.get_places);
-app.post('/add_place',routes.post_add_place);
 
 // create NodeJS HTTP server using 'app'
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -4,91 +4,42 @@
  * 
  * Routes contains the functions (callbacks) associated with request urls.
  */
-var request = require('request'); // library to make requests to remote urls
 
-var moment = require("moment"); // date manipulation library
-var placeModel = require("../models/place.js"); //db model
+//require the Twilio module and create a REST client
+var twilio = require('twilio')(process.env.TWILIO_ACCT_SID, process.env.TWILIO_AUTH_TOKEN);
+
 
 /*
 	GET /
 */
 exports.index = function(req, res) {
 	
-	console.log("main page requested");
+		
+	//Send an SMS text message
+	twilio.sendSms({
 
-	templateData = {
-		pageTitle : 'All Places'
-	};
+	    to:'+16467838457', // Any number Twilio can deliver to
+	    from: '+14155992671', // A number you bought from Twilio and can use for outbound communication
+	    body: 'word to your mother.' // body of the SMS message
 
-	res.render('index.html', templateData);
+	}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+	    if (!err) { // "err" is an error received during the request, if any
+
+	        // "responseData" is a JavaScript object containing data received from Twilio.
+	        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+	        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+	        console.log(responseData.from); // outputs "+14506667788"
+	        console.log(responseData.body); // outputs "word to your mother."
+
+	    } else {
+
+	    	console.log(err);
+	    }
+	});
+
+	res.send("okok");
+	// res.render('index.html', templateData);
 
 };
-
-exports.get_places = function(req, res) {
-
-	// query for all places
-	var query = placeModel.find({});
-	query.select('name geo_name geo');
-	query.exec(function(err, allPlaces){
-
-		if (err) {
-			res.send("Unable to query database for places").status(500);
-		};
-
-		console.log("retrieved " + allPlaces.length + " places from database");
-
-		//build and render template
-		var data = {
-			status : 'OK',
-			places : allPlaces
-		};
-
-		res.json(data)
-
-	});
-
-};
-
-// Add new place via POST
-// accepts name, latlng ("lat,lng") and geo_name
-// Returns JSON if request is XHR
-
-exports.post_add_place = function(req, res) {
-
-	// var name = req.body.place_name;
-	var latlng_str = req.body.latlng; // just a string
-	var latlng_array = latlng_str.split(","); // split string into array
-
-	var new_place = placeModel({
-		name : req.body.name,
-		geo : latlng_array, 
-		geo_name : req.body.geo_name
-	});
-	
-	// save to mongodb
-	new_place.save(function(err){
-		if (err) {
-			console.log("there was an error saving");
-			console.log(err);
-		} else {
-			console.log("New Place saved");
-			console.log(new_place);
-		}
-	});
-
-	// RESPONSE
-
-	// if req is XMLHTTPRequest (AJAX) then respond with JSON
-	if (req.xhr) {
-		var replyData = {
-			status : 'OK',
-			msg : 'Place added successfully.'
-		};
-		res.json(replyData);
-
-	} else {
-		res.redirect('/');	
-	}
-	
-
-}
